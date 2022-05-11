58e2f0cd55b47e9619e0991ad6f421e94e75fac1
Илья Задохин <russianvoodoo4@yandex.ru>
2022-05-11 21:58:33 +0000

    added ansible configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "yandex" {
-  token     = "t1.9euelZqPmZzGkJXKls6Zmsmei42WmO3rnpWayM2dx53OyIvMjZiUi8bOi4nl8_docjZs-e8cPxU9_d3z9yghNGz57xw_FT39.wb7dmyfAiA7-Rcue6-1cYz3shY2mwjwDG7MsfHEg9RCz0gegQWYp6TYEiFnzTLgn-pIm-xXq_PAITtaYEKbSAw"
+  token     = "t1.9euelZqJm5aVjJzPj5qTl5bGmY2Pk-3rnpWayM2dx53OyIvMjZiUi8bOi4nl8_cEKRFs-e94FDAY_d3z90RXDmz573gUMBj9._ebIpXIEMuyTUjRquDDNo_OSIHrkTN4A2G5OA4r5rBG-rRu5SQWBbNXoJ4Vv2p38228n-Wn4csm716lZ7HPLAw"
   cloud_id  = "b1g3jddf4nv5e9okle7p"
   folder_id = "b1gr94c4fmjnth6k8p36"
   zone      = "ru-central1-a"
@@ -24,13 +24,13 @@ resource "yandex_compute_instance" "vm-1" {
 
   boot_disk {
     initialize_params {
-      image_id = "fd8um17271hcbjirdqq1"
+      image_id = "fd80qm01ah03dkqb14lc"
     }
   }
 
   network_interface {
     subnet_id = "e9bdi1h4rfet06lt0dkq"
-    nat       = false
+    nat       = true
   }
 
   metadata = {
